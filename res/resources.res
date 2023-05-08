fba12530eb2b27ff7e39605a70a6e6259851aae7
Nixbyte <nixbyte@gmail.com>
2023-05-08 23:40:04 +0300

    Added working background and foreground

--- res/resources.res
+++ res/resources.res
@@ -1,1 +1,2 @@
 IMAGE bg1 "background1.png" BEST ALL 
+IMAGE fg1 "foreground1.png" BEST ALL 
--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include <genesis.h>
-#include <resources.res>
+#include <resources.h>
 
 int main()
 {
 
 
 u16 ind = TILE_USER_INDEX;
+
 PAL_setPalette(PAL0, bg1.palette->data, DMA);
-/*VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);*/
+
+VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
+
+ind += bg1.tileset->numTile;
+
+PAL_setPalette(PAL1, fg1.palette->data, DMA);
+
+VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
+
+ind += fg1.tileset->numTile;
 
 	while(1)
 	{        
