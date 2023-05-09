e084f50873dbd6374b6b9f90ca73b9093def398f
Nixbyte <nixbyte@gmail.com>
2023-05-09 20:35:22 +0300

    Added scrolling

--- src/main.c
+++ src/main.c
@@ -1,27 +1,32 @@
 #include <genesis.h>
 #include <resources.h>
 
-int main()
-{
-
 
 u16 ind = TILE_USER_INDEX;
+int hscroll_offset = 0;
+int hscroll_offset_foreground = 0;
 
-PAL_setPalette(PAL0, bg1.palette->data, DMA);
+int main()
+{
+	PAL_setPalette(PAL0, bg1.palette->data, DMA);
+	VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
+	ind += bg1.tileset->numTile;
+	PAL_setPalette(PAL1, fg1.palette->data, DMA);
+	VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
+	ind += fg1.tileset->numTile;
 
-VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
 
-ind += bg1.tileset->numTile;
-
-PAL_setPalette(PAL1, fg1.palette->data, DMA);
-
-VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE,ind), 0,0,FALSE,TRUE);
-
-ind += fg1.tileset->numTile;
+	VDP_setScrollingMode(HSCROLL_PLANE,VSCROLL_PLANE);
 
 	while(1)
-	{        
-		VDP_waitVSync();
+	{    
+		
+		VDP_setHorizontalScroll(BG_B, hscroll_offset); 
+		VDP_setHorizontalScroll(BG_A, hscroll_offset_foreground); 
+		hscroll_offset -= 1; 
+		hscroll_offset_foreground -=2;
+		
+		SYS_doVBlankProcess();
 	}
 
 	return (0);
